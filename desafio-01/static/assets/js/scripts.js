0f6b299c9f6f65dd99df53ed90550218e0909598
pedro-loures <PLoures2@hotmail.com>
2022-07-28 01:21:26 -0300

    insert params

--- desafio-01/static/assets/js/scripts.js
+++ desafio-01/static/assets/js/scripts.js
@@ -38,20 +38,12 @@ dropArea.addEventListener("drop", (event)=>{
   showFile(); //calling function
 });
 
-// function showFile(){
-//   let fileType = file.type; //getting selected file type
-//   let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
-//   if(validExtensions.includes(fileType)){ //if user selected file is an image file
-//     let fileReader = new FileReader(); //creating new FileReader object
-//     fileReader.onload = ()=>{
-//       let fileURL = fileReader.result; //passing user file source in fileURL variable
-//       let imgTag = `<img src="${fileURL}" alt="">`; //creating an img tag and passing user selected file source inside src attribute
-//       dropArea.innerHTML = imgTag; //adding that created img tag inside dropArea container
-//     }
-//     fileReader.readAsDataURL(file);
-//   }else{
-//     alert("This is not an Image File!");
-//     dropArea.classList.remove("active");
-//     dragText.textContent = "Drag & Drop to Upload File";
-//   }
-// }
+function showFile(){
+  let fileType = file.type; //getting selected file type
+  let validExtensions = ["image/jpeg", "image/jpg", "image/png"]; //adding some valid image extensions in array
+  if(!validExtensions.includes(fileType)){ //if user selected file is an image file
+    alert("This is not an Image File!");
+    dropArea.classList.remove("active");
+    dragText.textContent = "Drag & Drop to Upload File";
+  }
+}
